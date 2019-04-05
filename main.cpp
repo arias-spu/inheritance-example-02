2ea3662f7f043c5d06643d482b0ce169c9a86342
Carlos R. Arias <arias@spu.edu>
2019-04-05 11:35:34 -0700

    Used Pointers
    Using a vector of pointers, but still no luck

--- main.cpp
+++ main.cpp
@@ -9,15 +9,19 @@ using std::endl;
 using std::vector;
 
 int main(){
-	vector<Shape> shapes;
+	vector<Shape*> shapes;
 
-	shapes.push_back(Shape());
-	shapes.push_back(Square(5));
-	shapes.push_back(Rectangle(1, 3));
+	shapes.push_back(new Shape());
+	shapes.push_back(new Square(5));
+	shapes.push_back(new Rectangle(1, 3));
 	// shapes.push_back(string("Hello")); will not compile because string "is not a" Shape
 
 	for (size_t i = 0; i < shapes.size(); i++) {
-		cout << shapes[i].ToString() << endl;
+		cout << shapes[i]->ToString() << endl;
+	}
+
+	for (size_t i = 0; i < shapes.size(); i++) {
+		delete shapes.at(i);
 	}
 
 	return 0;
